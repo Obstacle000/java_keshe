a87a58c406a7f77d5d8cfa5b017ada3e5fa0e71a
Obstacle <202400300052@sdu.edu.cn>
2025-04-10 18:38:07 +0800

    MunuController弄懂了

--- java-fx/src/main/java/com/teach/javafx/controller/base/MenuController.java
+++ java-fx/src/main/java/com/teach/javafx/controller/base/MenuController.java
@@ -204,6 +204,9 @@ public class MenuController extends ToolController{
     }
     @FXML // 表单保存按钮
     protected void onSubmitButtonClick() {
+        if(editNode == null) {
+            throw new RuntimeException("请先选择一个节点的业务");
+        }
         editNode.setId(Integer.parseInt(nodeIdField.getText()));
         editNode.setValue(nodeNameField.getText());
         editNode.setTitle(nodeTitleField.getText());
