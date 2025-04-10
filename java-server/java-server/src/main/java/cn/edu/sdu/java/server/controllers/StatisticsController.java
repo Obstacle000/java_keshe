9c683c830d7350fdbe75878f4120a7890a1a0ad4
Obstacle <202400300052@sdu.edu.cn>
2025-04-10 18:16:58 +0800

    studentController看完了,正在看MunuController

---
+++ java-server/java-server/src/main/java/cn/edu/sdu/java/server/controllers/StatisticsController.java
@@ -0,0 +1,29 @@
+package cn.edu.sdu.java.server.controllers;
+
+import cn.edu.sdu.java.server.payload.request.DataRequest;
+import cn.edu.sdu.java.server.payload.response.DataResponse;
+import cn.edu.sdu.java.server.services.StatisticsService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/statistics")
+@Slf4j
+public class StatisticsController {
+    private final StatisticsService statisticsService;
+    public StatisticsController(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
+    @PostMapping("/getMainPageData")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('STUDENT') or hasRole('TEACHER')")
+    public DataResponse getMainPageData(@Valid @RequestBody DataRequest dataRequest) {
+        log.info("halou");
+        return statisticsService.getMainPageData(dataRequest);
+    }
+
+}
